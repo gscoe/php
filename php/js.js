a43295f1c3daa1fad90e4804aa9af8cf20f980e0
gscoe <100785135+gscoe@users.noreply.github.com>
2022-03-03 17:07:11 +0800

    Add files via upload

---
+++ php/js.js
@@ -0,0 +1,37 @@
+<script src="js/jquery-2.1.3.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script>
+    function toggle(){
+        $.getJSON("php/get.php",function (res) {
+            if(res['flag']){
+                $('li.toggle').hide();
+                $('li.toggle1').show();
+                $('#nicheng').html(res['nc'])
+            }else {
+                $('li.toggle').show();
+                $('li.toggle1').hide();
+            }
+        });
+    }
+    toggle();
+    $('#form_login').submit(function (e) {
+        e.preventDefault();
+        var data=$('#form_login').serialize();
+        $.getJSON('php/login.php',data,function (res) {
+            if(res==3){
+                toggle();
+                $('#loginer').modal('hide');
+            }else if(res==2){
+                $('#info').html('用户名或密码有误')
+            }else {
+                $('#info').html('验证码有误')
+            }
+        })
+    });
+    $('#logout').click(function () {
+        confirm('确定要注销？');
+        $.getJSON('php/logout.php',function (res) {
+            if(res)toggle();
+        })
+    })
+    </script>
