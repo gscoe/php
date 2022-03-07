c6bae36540346fb707059d934c9cdce8f6d35a33
gscoe <100785135+gscoe@users.noreply.github.com>
2022-03-07 14:07:45 +0800

    Update logout.php

--- php/logout.php
+++ php/logout.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
-unset($_SESSION['username']);//销毁用户名
-unset($_SESSION['nc']);//销毁昵称
+unset($_SESSION['username']);//注销用户名
+unset($_SESSION['nc']);//注销昵称
 echo json_encode(true);//返回结果
