ffc9b97d9a46a3987c43378ebaf5a23481fecabf
gscoe <100785135+gscoe@users.noreply.github.com>
2022-03-13 17:11:03 +0800

    Update login.php

--- php/login.php
+++ php/login.php
@@ -10,7 +10,7 @@ if($yzm==$_SESSION['vCode']){/*当用户输入的验证码和图片验证码相�
     $row = $result->fetch();/*读取从数据库获取到的数据*/
     if ($row) {/*如果数据存在，即用户登录成功*/
         $_SESSION['username'] = $row['username'];
-        /*将用户名和昵称存在服务器，可以多个页面使用*/
+        /*将用户名和昵称存在服务器，可以在多个页面使用*/
         $_SESSION['nc'] = $row['nc'];
         $flag=3;
     }else{/*用户名或密码错误*/
