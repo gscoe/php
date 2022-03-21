9cf8189598a98d8d68f31a243489aaaef85a232e
gscoe <100785135+gscoe@users.noreply.github.com>
2022-03-21 20:49:32 +0800

    Update get.php

--- php/get.php
+++ php/get.php
@@ -1,7 +1,7 @@
 <?php
 include_once ("connect.php");
 session_start();/*开启会话*/
-if (isset($_SESSION['username'])){
+if (isset($_SESSION['username1'])){
    /* 判断用户会话里用户名是否存在,用户是否登录*/
     $json['nc']=$_SESSION['nc'];/*把昵称存起来，一会返回给首页*/
     $json['flag']=true;/*用户已经登录，标志flag为true*/
