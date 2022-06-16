bceee2876edc90f36649df09837eea45ecaef69a
gscoe <100785135+gscoe@users.noreply.github.com>
2022-06-16 10:05:10 +0800

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,3 +1,3 @@
 <?php
-$link=new PDO("mysql:host=localhost;port=3306;dbname=db","root","wodesqlmima");
+$link=new PDO("mysql:host=localhost;port=3316;dbname=db","root","wodesqlmima");
 $link->query("set names utf8");
